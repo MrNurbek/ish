9bcab4d5ecb0c294325cf32717eb3022d33700c0
Nurbek <xamrayevnurbek0@gmail.com>
2024-04-23 10:30:01 +0500

    gg

--- page/admin.py
+++ page/admin.py
@@ -27,7 +27,7 @@ class FileEmployeeAdmin(admin.ModelAdmin):
 
 
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('id', 'email', 'username', 'is_staff', 'last_name', 'password', 'unvoni', 'xonasi')
+    list_display = ('id', 'email', 'username', 'is_staff', 'last_name', 'password', 'unvoni', 'xonasi','image')
 
 
 class MessageAdmin(admin.ModelAdmin):
